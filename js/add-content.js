9e7740afee43697724e3e353ed03a7c94b1aef68
yamama95 <yamamayahia5@gmail.com>
2020-02-06 00:29:47 +0200

    new one

--- js/add-content.js
+++ js/add-content.js
@@ -1,16 +1,5 @@
-var today = new Date();
-var hourNOw = today.getHours();
-var greeting;
-
-if (hourNOw > 18) {
-    greeting = 'Good evening!';
 
 
-} else if (hourNOw > 0) {
-    greeting = 'Good morning!';
+prompt("what is your name ");
 
-} else {
-    greeting = 'welcome!';
-
-}
-document.write('<h3>'+ greeting + '</h3>');
+alert ("hello everyone");
