7baeb7e75401abccb7d20af395ceec94254afd4e
yamama95 <yamamayahia5@gmail.com>
2020-02-06 22:29:13 +0200

    laast

--- js/add-content.js
+++ js/add-content.js
@@ -1,5 +1,35 @@
 
 
-prompt("what is your name ");
+var age = prompt("what is your age ");
+if (age <18) {
+    alert ("we recommend simple books for you");
+} else {
+    alert ("we recommend mental books for you");
+}
 
-alert ("hello everyone");
+var showOrder = function(){
+    var userOrder; 
+
+var orderImage = '';
+while(userOrder !=="mental book" && userOrder !== "healthy book"){
+    userOrder = prompt("please choose one of both!");
+
+}
+var numberofbooks;
+numberofbooks = prompt("how many books you want to buy?");
+for(var i =0 ; i < numberofbooks ; i++ ){
+    console.log("index is" + i);
+    if (userOrder === "mental book"){
+        orderImage  = orderImage + '< img src = "https://tse3.mm.bing.net/th?id=OIP.WB56G3InZtgmoCrUmcy4ZQHaDq&pid=Api&H=78&W=160">'
+    }
+        
+     else if (userOrder === "healthy book"){
+        orderImage = orderImage + '<img src= "https://tse4.mm.bing.net/th?id=OIP.RJJskbrU41DZ_Zcpc9aknwAAAA&pid=Api&H=205&W=160">'
+    } 
+}
+    
+    
+    return orderImage;
+}
+
+document.write('<p>'+ showOrder() +'</p>');
